21ef3d006e9e823698ae30226fb3730fceba61f4
Harshit shukla <158827992+harsshhit@users.noreply.github.com>
2024-06-18 18:50:47 +0000

    updated styles

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -22,6 +22,7 @@ const Messages = () => {
 
   return (
     <div className="messages">
+      
       {messages.map((m) => (
         <Message message={m} key={m.id} />
       ))}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const { currentUser } = useContext(AuthContext);
   return (
     <div className="navbar">
-      <span className="logo">ChatHere</span>
+      <span className="logo">ChatHere App</span>
       <div className="user">
         <img src={currentUser.photoURL} alt="" />
         <span>{currentUser.displayName}</span>
